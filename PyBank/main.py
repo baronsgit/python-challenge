9a3aee34ac1b25e82e575ecde8ecb5cc1d76b566
Baron Stubblefield <Baron.Stubblefield@kmob.com>
2023-04-17 17:54:29 -0700

    budget update

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,6 +1,22 @@
 import os
 import csv
 
-monthsTotal=0
+current = os.path.join(".","Resources","budget_data.csv")
+print(str(current))
 
-# with os.
+# dirs = os.path("./PyBank")
+
+# for dir in dirs:
+#     print(dir)
+
+# budget_csv = os.path.join("..","Resources","budget_data.csv")
+
+# with open(budget_csv) as cvs_doc:
+#     fileReader = csv.reader(cvs_doc, delimiter=",")
+    
+#     fileReader=next(cvs_doc)
+
+# print(f"Header {fileReader}")
+
+# c:/Projects/DataAnalytics/repos/python-challenge/PyBank/main.py..\Resources\budget_data.csv
+# C:\Projects\DataAnalytics\repos\python-challenge\PyBank\Resources\budget_data.csv
